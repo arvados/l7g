148680ae18617f7f490033d3be238845da539246
abram <abram@lightning-dev1.shell.su92l.arvadosapi.com>
2017-08-21 19:40:16 +0000

    no issue #
    comand line options and help
    
    Arvados-DCO-1.1-Signed-off-by: Abram Connelly <abram@curoverse.com>

--- tools/which-ref/which-ref.cpp
+++ tools/which-ref/which-ref.cpp
@@ -1,37 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 
 #include "asm_ukk.h"
 
 #include <string>
 #include <vector>
 
+#define WHICH_REF_VERSION "0.1.0"
+
+typedef struct opt_type {
+  int debug;
+  int show_all_score;
+  int verbose;
+  int version;
+} opt_t;
+
+static struct option long_options[] = {
+  {"help",                no_argument,        NULL, 'h'},
+  {"version",             no_argument,        NULL, 'v'},
+  {"verbose",             no_argument,        NULL, 'V'},
+  {0,0,0,0}
+};
+
+
 void print_usage() {
   printf("\n");
-  printf("  which-ref [query-seq0] [query-seq1] [query-seq2] ... [query-seqN] [ref-seq]\n");
+  printf("usage:\n");
   printf("\n");
+  printf("    which-ref [-h] [-v] [-V] [query-seq0] [query-seq1] [query-seq2] ... [query-seqN] [ref-seq]\n");
+  printf("\n");
+  printf("  [-h]        Print help (this screen)\n");
+  printf("  [-v]        Print version\n");
+  printf("  [-V]        Verbose flag\n");
+  printf("\n");
+}
+
+void print_version() {
+  printf("version: %s\n", WHICH_REF_VERSION);
+}
+
+void init_opt(opt_t *opt) {
+  opt->debug = 0;
+  opt->show_all_score = 0;
+  opt->verbose = 0;
 }
 
 int main(int argc, char **argv) {
   FILE *fp;
-  int i, k, ch;
+  int i, k;
   std::vector< std::string > ref_fns, ref_seq;
   std::string ifn, seq, tseq;
   int min_score, min_idx;
   int loc_debug = 0;
 
+  int ch, option_index;
+  opt_t opt;
+
   std::vector< int > score;
 
+  while ((ch = getopt_long(argc, argv, "hvVA", long_options, &option_index))!=-1) switch(ch) {
+    case 0:
+      fprintf(stderr, "sanity error, invalid optino to parse, exiting\n");
+      exit(-1);
+      break;
+    case 'v':
+      print_version();
+      exit(0);
+      break;
+    case 'V':
+      opt.verbose=1;
+      break;
+    case 'A':
+      opt.show_all_score=1;
+      break;
+    default:
+    case 'h':
+      print_usage();
+      exit(0);
+      break;
+  }
+
+  if (argc>optind) {
+    for (i=0; i<(argc-optind-1); i++) {
+      ref_fns.push_back( argv[optind+i] );
+    }
+    ifn = argv[optind+i];
+  }
+
+  if (ref_fns.size()==0) {
+    printf("Provide input sequences to compare\n");
+    print_usage();
+    exit(0);
+  }
+
+  /*
   if (argc<3) {
     print_usage();
     exit(-1);
   }
 
   for (i=1; i<(argc-1); i++) {
-    ref_fns.push_back( argv[i] );
   }
 
   ifn = argv[argc-1];
+  */
 
   //DEBUG
   //
